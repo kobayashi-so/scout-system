e0d14a0f656e17ad5e3827c245aaa6de5cd4b8af
Sora Kobayashi <kobayashi-so@lakeel.com>
2026-05-27 15:44:39 +0900

    一覧画面を少し作成

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [vue()],
+    cacheDir: "node_modules/.vite",
     server: {
         host: '0.0.0.0',
         port: 5173,
