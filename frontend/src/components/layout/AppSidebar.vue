d834cd1c2d10a8cadbad7cfac25fe9ed43ce219d
Sora Kobayashi <kobayashi-so@lakeel.com>
2026-05-29 17:55:53 +0900

    css修正

--- frontend/src/components/layout/AppSidebar.vue
+++ frontend/src/components/layout/AppSidebar.vue
@@ -3,7 +3,7 @@
     <h1 class="mt-6 mb-6 text-lg font-bold">Scout System</h1>
 
     <nav class="space-y-2">
-        <RouterLink
+      <RouterLink
         to="/list"
         class="block rounded-lg px-3 py-2 text-sm font-medium transition hover:bg-slate-100"
         active-class="bg-slate-900 text-white hover:bg-slate-900"
